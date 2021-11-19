95f4a106ce3478889403c0a1004350a109c9ff65
Yaya <bedourayannis@hotmail.com>
2021-11-19 02:14:22 +0100

    Updates

---
+++ precache-manifest.53ce89074cdc4427456dee25006c6bee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e53750655ba91a91399e9c2080f74d30",
+    "url": "/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "74703593d37f548c85d5",
+    "url": "/react-monsters-rolodex/static/css/main.d166f474.chunk.css"
+  },
+  {
+    "revision": "1d722c262500ea01afd4",
+    "url": "/react-monsters-rolodex/static/js/2.82cd4665.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/react-monsters-rolodex/static/js/2.82cd4665.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e139292a96b6d29a883",
+    "url": "/react-monsters-rolodex/static/js/3.9d75d10a.chunk.js"
+  },
+  {
+    "revision": "74703593d37f548c85d5",
+    "url": "/react-monsters-rolodex/static/js/main.a8113908.chunk.js"
+  },
+  {
+    "revision": "7b34efba1b818815514b",
+    "url": "/react-monsters-rolodex/static/js/runtime-main.7c5da275.js"
+  }
+]);
